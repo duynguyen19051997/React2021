c335e85038fc3c7bcafa6aec7c931902d3f39dd8
duynguyen19051997 <nguyenvuanhduy1905@gmail.com>
2021-07-01 11:20:36 +0700

    ReactJS 2021

--- 00-starting-project/src/App.js
+++ 00-starting-project/src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Nguyen Vu Anh Duy --- React JavaScript Course 2021
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
