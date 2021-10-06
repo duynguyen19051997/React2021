553f6d5540ffa4a5b8e195ac6d2db53e86dc1a7e
duynguyen19051997 <nguyenvuanhduy1905@gmail.com>
2021-10-06 17:07:11 +0700

    01 class-based component

---
+++ 01-class-based-starting/src/components/UserFinder.js
@@ -0,0 +1,76 @@
+import { Fragment, Component } from "react";
+
+import Users from "./Users";
+
+import classes from "./UserFinder.module.css";
+import UsersContext from "../store/users-context";
+
+// const DUMMY_USERS = [
+//   { id: "u1", name: "Max" },
+//   { id: "u2", name: "Manuel" },
+//   { id: "u3", name: "Julie" },
+// ];
+
+class UserFinder extends Component {
+  static contextTypes = UsersContext;
+  constructor(props) {
+    super();
+    this.state = { filteredUsers: [], searchTerm: "" };
+  }
+
+  componentDidMount() {
+    // sent request server and get data
+    this.setState({ filteredUsers: this.context.users });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchTerm !== this.state.searchTerm) {
+      this.setState({
+        filteredUsers: this.context.users.filter((user) =>
+          user.name.includes(this.state.searchTerm)
+        ),
+      });
+    }
+  }
+
+  searchChangeHandler(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className={classes.finder}>
+          <input type="search" onChange={this.searchChangeHandler.bind(this)} />
+        </div>
+        <Users users={this.state.filteredUsers} />
+      </Fragment>
+    );
+  }
+}
+
+// const UserFinder1 = () => {
+//   const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
+//   const [searchTerm, setSearchTerm] = useState("");
+
+//   useEffect(() => {
+//     setFilteredUsers(
+//       DUMMY_USERS.filter((user) => user.name.includes(searchTerm))
+//     );
+//   }, [searchTerm]);
+
+//   const searchChangeHandler = (event) => {
+//     setSearchTerm(event.target.value);
+//   };
+
+//   return (
+//     <Fragment>
+//       <div className={classes.finder}>
+//         <input type="search" onChange={searchChangeHandler} />
+//       </div>
+//       <Users users={filteredUsers} />
+//     </Fragment>
+//   );
+// };
+
+export default UserFinder;
