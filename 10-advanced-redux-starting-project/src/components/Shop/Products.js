fd5ed9f3b9922df449aae6b56137652582e1e2c4
duynguyen19051997 <nguyenvuanhduy1905@gmail.com>
2021-12-06 17:03:05 +0700

    advanced redux

--- 10-advanced-redux-starting-project/src/components/Shop/Products.js
+++ 10-advanced-redux-starting-project/src/components/Shop/Products.js
@@ -1,16 +1,27 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
+
+const DUMMY_PRODUCTS = [
+  { id: "p1", price: 5, title: "Book", description: "A wonderful book" },
+  { id: "p2", price: 6, title: "Car", description: "A wonderful car" },
+  { id: "p3", price: 7, title: "Laptop", description: "A wonderful laptop" },
+  { id: "p4", price: 8, title: "Phone", description: "A wonderful phone" },
+];
 
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((item) => (
+          <ProductItem
+            key={item.id}
+            id={item.id}
+            title={item.title}
+            price={item.price}
+            description={item.description}
+          />
+        ))}
       </ul>
     </section>
   );
